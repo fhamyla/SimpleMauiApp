8cd13de47355fe4ee6fa442620ae68d82151c406
fhamyla <fhamyla.devera@gmail.com>
2024-12-14 18:48:14 +0800

    Add project files.

---
+++ SimpleMauiApp/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace SimpleMauiApp;
+
+public partial class MainPage : ContentPage
+{
+    private int clickCount = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnButtonClicked(object sender, EventArgs e)
+    {
+        clickCount++;
+        countLabel.Text = $"Button clicked {clickCount} times";
+    }
+}
