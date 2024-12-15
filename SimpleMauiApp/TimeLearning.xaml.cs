42c1661410100e64f10287866fd44bd0c6559985
fhamyla <fhamyla.devera@gmail.com>
2024-12-15 21:40:17 +0800

    added time notification

--- SimpleMauiApp/TimeLearning.xaml.cs
+++ SimpleMauiApp/TimeLearning.xaml.cs
@@ -1,36 +1,59 @@
-namespace SimpleMauiApp;
+using System;
+using Microsoft.Maui.Controls;
 
-public partial class TimeLearning : ContentPage
+namespace SimpleMauiApp
 {
-	public TimeLearning()
-	{
-		InitializeComponent();
-	}
-    private async void OnContinueClicked(object sender, EventArgs e)
+    public partial class TimeLearning : ContentPage
     {
-        string timeLevel = string.Empty;
-
-        if (LaidBackOption.IsChecked)
+        public TimeLearning()
         {
-            timeLevel = "5-10 minutes a day";
-        }
-        else if (ConsistentOption.IsChecked)
-        {
-            timeLevel = "10-15 minutes everyday";
-        }
-        else if (SincereOption.IsChecked)
-        {
-            timeLevel = "20 minutes everyday";
+            InitializeComponent();
         }
 
-        if (!string.IsNullOrEmpty(timeLevel))
+        private async void OnContinueClicked(object sender, EventArgs e)
         {
-            await DisplayAlert("Time Learning", $"You selected: {timeLevel}", "OK");
-            await Navigation.PushAsync(new Lesson());
+            string timeLevel = string.Empty;
+            TimeSpan notificationTime = TimeSpan.Zero;
+
+            if (LaidBackOption.IsChecked)
+            {
+                timeLevel = "5-10 minutes a day";
+                notificationTime = TimeSpan.FromMinutes(10);
+            }
+            else if (ConsistentOption.IsChecked)
+            {
+                timeLevel = "10-15 minutes everyday";
+                notificationTime = TimeSpan.FromMinutes(15);
+            }
+            else if (SincereOption.IsChecked)
+            {
+                timeLevel = "20 minutes everyday";
+                notificationTime = TimeSpan.FromMinutes(20);
+            }
+
+            if (!string.IsNullOrEmpty(timeLevel))
+            {
+                await DisplayAlert("Time Learning", $"You selected: {timeLevel}", "OK");
+
+                // Schedule notification using the Dispatcher
+                ScheduleNotification(notificationTime);
+
+                // Navigate to the Lesson page
+                await Navigation.PushAsync(new Lesson());
+            }
+            else
+            {
+                await DisplayAlert("Error", "Please select an option to continue.", "OK");
+            }
         }
-        else
+
+        private void ScheduleNotification(TimeSpan delay)
         {
-            await DisplayAlert("Error", "Please select an option to continue.", "OK");
+            // Use the Dispatcher to schedule the notification
+            Dispatcher.DispatchDelayed(delay, () =>
+            {
+                DisplayAlert("Reminder", "It's time for your learning session!", "OK");
+            });
         }
     }
 }
